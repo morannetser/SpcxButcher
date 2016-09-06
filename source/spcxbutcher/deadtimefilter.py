96ab0fc4f336551f262cd61b58af0bceb02538fa
yoav <haarcuba@gmail.com>
2016-09-06 23:32:58 +0300

    redesign deadtime to be a true filter class

--- source/spcxbutcher/deadtimefilter.py
+++ source/spcxbutcher/deadtimefilter.py
@@ -1,10 +1,15 @@
-def deadtimeFilter( records, deadtime ):
-    iterator = iter( records )
-    previous = next( iterator )
-    result = [ previous ]
-    for current in iterator:
-        if current.timestamp - previous.timestamp > deadtime:
-            result.append( current )
-            previous = current
+class DeadtimeFilter:
+    def __init__( self, deadtime ):
+        self._deadtime = deadtime
+        self._previous = None
 
-    return result
+    def __call__( self, current ):
+        if self._previous is None:
+            self._previous = current
+            return True
+
+        if current.timestamp - self._previous.timestamp > self._deadtime:
+            self._previous = current
+            return True
+
+        return False
--- test/deadtime_filter_test.py
+++ test/deadtime_filter_test.py
@@ -8,15 +8,15 @@ class FakeEvent:
 class DeadtimeFilterTest( unittest.TestCase ):
     def test_records_should_be_at_least_deadtime_apart( self ):
         records = [ FakeEvent( timestamp ) for timestamp in [ 1, 3, 5, 8, 9, 10, 20, 22, 25, 40, 41, 42] ]
-        filtered5 = deadtimefilter.deadtimeFilter( records, 5 )
-        self.assertEqual( [ 1, 8, 20, 40 ], [ record.timestamp for record in filtered5 ] )
+        filter5 = deadtimefilter.DeadtimeFilter( 5 )
+        self.assertEqual( [ 1, 8, 20, 40 ], [ record.timestamp for record in filter( filter5, records ) ] )
 
-        filtered3 = deadtimefilter.deadtimeFilter( records, 3 )
-        self.assertEqual( [ 1, 5, 9, 20, 25, 40 ], [ record.timestamp for record in filtered3 ] )
+        filter3 = deadtimefilter.DeadtimeFilter( 3 )
+        self.assertEqual( [ 1, 5, 9, 20, 25, 40 ], [ record.timestamp for record in filter( filter3, records ) ] )
 
-        filtered0 = deadtimefilter.deadtimeFilter( records, 0 )
-        self.assertEqual( [ 1, 3, 5, 8, 9, 10, 20, 22, 25, 40, 41, 42], [ record.timestamp for record in filtered0 ] )
+        filter0 = deadtimefilter.DeadtimeFilter( 0 )
+        self.assertEqual( [ 1, 3, 5, 8, 9, 10, 20, 22, 25, 40, 41, 42], [ record.timestamp for record in filter( filter0, records ) ] )
 
         INFINITY = 1e100
-        filteredInfinity = deadtimefilter.deadtimeFilter( records, INFINITY )
-        self.assertEqual( [1,], [ record.timestamp for record in filteredInfinity ] )
+        filterInfinity = deadtimefilter.DeadtimeFilter( INFINITY )
+        self.assertEqual( [1,], [ record.timestamp for record in filter( filterInfinity, records ) ] )
