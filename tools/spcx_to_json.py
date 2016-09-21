4e77bcc1b42cc9a8ee1cab8e155a4ccd51cf22e4
yoav <haarcuba@gmail.com>
2016-09-22 00:40:17 +0300

    improve memory performance using custom JSONEncoder

--- tools/spcx_to_json.py
+++ tools/spcx_to_json.py
@@ -9,8 +9,19 @@ def eventTuple( event ):
 
 def spcDictionary( spc ):
     return { 'raw': spc.raw,
-                'timePerBin': spc.timePerBin,
-                'events': [ eventTuple( event ) for event in spc ] }
+             'timePerBin': spc.timePerBin,
+             'events': [ eventTuple( event ) for event in spc ] }
+
+class JSONEncoder( json.JSONEncoder ):
+    def default( self, thing ):
+        if isinstance( thing, spcxbutcher.spcxparser.SPCXParser ):
+            spcs = list( thing )
+            return { "spcs": spcs }
+        elif isinstance( thing, spcxbutcher.spc.SPC ):
+            return spcDictionary( thing )
+
+        return json.JSONEncoder.default( self, thing )
+
 
 
 parser = argparse.ArgumentParser()
@@ -20,6 +31,4 @@ arguments = parser.parse_args()
 logging.basicConfig( level = logging.DEBUG )
 
 parsed = spcxbutcher.spcxparser.SPCXParser( arguments.spcxfile )
-spcs = [ spcDictionary( spc ) for spc in parsed ]
-
-json.dump( spcs, sys.stdout )
+json.dump( parsed, sys.stdout, cls=JSONEncoder )
