2a3bf4c73b08041432edbfdd8f6845f1b5a36fa4
yoav <haarcuba@gmail.com>
2016-09-21 23:09:57 +0300

    tool to convert spcx to json

---
+++ tools/spcx_to_json.py
@@ -0,0 +1,25 @@
+import logging
+import json
+import argparse
+import spcxbutcher.spcxparser
+import sys
+
+def eventTuple( event ):
+    return event.gap, event.channel, event.timestamp
+
+def spcDictionary( spc ):
+    return { 'raw': spc.raw,
+                'timePerBin': spc.timePerBin,
+                'events': [ eventTuple( event ) for event in spc.events ] }
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument( 'spcxfile' )
+arguments = parser.parse_args()
+
+logging.basicConfig( level = logging.DEBUG )
+
+parsed = spcxbutcher.spcxparser.SPCXParser( arguments.spcxfile )
+spcs = [ spcDictionary( spc ) for spc in parsed ]
+
+json.dump( spcs, sys.stdout )
